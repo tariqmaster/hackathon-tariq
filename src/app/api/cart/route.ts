f886fd9d07248a8da960addada7059f7ae7fbe74
Muhammad Tariq <tariqchemengr@gmail.com>
2023-07-22 00:25:40 +0500

    fetch command commented out
    fetch command creating problem.

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,52 +1,3 @@
-// import { NextRequest,NextResponse } from "next/server";
-// import {db, cartTable} from "../../../lib/drizzle";
-// import {v4 as uuid} from "uuid";
-// import { cookies } from "next/headers";
-
-// export const GET = async (request: Request) => {
-   
-//    // await db`create ta`
-//    try{
-//         const res = await db.select().from(cartTable)
-//         return NextResponse.json({res})
-//    }
-//    catch(error)
-//    {
-//     console.log(error);
-//     return NextResponse.json({message:"Something went wrong."})
-//    }
-// }
-
-// export const POST = async (request: Request) => {
-
-//     const req = await request.json();
-
-//    // cookies().get("user_id")
-
-//      const uid = uuid(); //randomly generated user id!
-//      const setCookies = cookies();
-     
-//      setCookies.set("user_id", uid);
-//      const user_id = cookies().get("user_id")?.value;
-// //  if(!user_id){
-// //  setCookies.set("user_id", uid)
-// //  }
-
-
-//     try{
-//          const res = await db.insert(cartTable).values({
-//             product_id: req.product_id,
-//             quantity: 1,
-//             user_id: user_id as string,
-//          }).returning();
-//          return NextResponse.json({res})
-//     }
-//     catch(error)
-//     {
- 
-//     }
-//  }
-
 import { cartTable, db } from "@/lib/drizzle";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -54,7 +5,7 @@ import { v4 as uuid} from "uuid";
 import { eq } from "drizzle-orm";
 
 
-fetch(`http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}`)
+//  fetch(`http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}`)
 
 export const GET = async (request: NextRequest )=>{
 
@@ -66,11 +17,9 @@ export const GET = async (request: NextRequest )=>{
         return NextResponse.json({res})
     } catch (error) {
         console.log(error);
-        return NextResponse.json({message: "Something went wrong"})
-        
+        return NextResponse.json({message: "Something went wrong"}) 
     }
 }
-
 
 export const POST = async (request: NextRequest )=>{
     const req = await request.json()
